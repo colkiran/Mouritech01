0122631744b87351665e49d6b3c5102e523400a0
Kiran <colkiran@gmail.com>
2022-09-26 10:25:59 +0530

    Initial commit

---
+++ Day01/sample.py
@@ -0,0 +1,11 @@
+#! c:\python39
+
+print("hello world")
+print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+
+print("Module Name :", __name__)
