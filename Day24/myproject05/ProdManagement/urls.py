59826e5743b6c9cfb55d503ce7932fe29e7956f4
Kiran <colkiran@gmail.com>
2022-11-01 10:27:58 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ Day24/myproject05/ProdManagement/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.add_show, name="add"),
+    path("updatedata/<int:id>", views.updatedata, name="updatedata"),
+    path("deletdata/<int:id>", views.deletedata, name="deletedata")
+
+]
